4ad14daa8edf272bb4c0c27dffcfe26d19832e1e
fmskkottarakara <fmservicekottarakkara@gmail.com>
2025-03-05 22:11:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider } from './context.js';
 import './index.css';
 import Home from './pages/Home/Home';
-import MainHome from './MainHomeComp/frontend/js/MainHome';
+import MainHome from './MainHomeComp/frontend/MainHome';
 import About from "./pages/About/About";
 import BookList from "./Components/BookList/BookList";
 import BookDetails from "./Components/BookDetails/BookDetails";
 import Community from "./Community/components/Community.js";
 import CreateProfile from "./Community/components/CreateProfile.js";
 import ProfileCard from "./Community/components/UserProfile.jsx";
-import auth from './Login/public/js/auth.js'; // Import the AuthPage component
 import "./Community/App.css";
-import AuthPage from './Login/public/js/auth.js';
-import SearchEngine from './pages/SearchEngine/SearchEngine';
-import PrivateMessages from './pages/PrivateMessages/PrivateMessages';
-import BookChannels from './pages/BookChannels/BookChannels';
-import Polls from './pages/Polls/Polls';
+import AuthPage from './Login/auth.js';
+// import SearchEngine from './pages/SearchEngine/SearchEngine';
+// import PrivateMessages from './pages/PrivateMessages/PrivateMessages';
+// import BookChannels from './pages/BookChannels/BookChannels';
+// import Polls from './pages/Polls/Polls';
 
 // Custom protected route component
 const ProtectedRoute = ({ children }) => {
@@ -29,15 +28,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
     <BrowserRouter>
-      <div className="app-container">
-        <div className="main-content">
           <Routes>
             {/* Authentication Routes */}
             <Route path="/login" element={<AuthPage initialView="login" />} />
-            <Route path="/register" element={<AuthPage initialView="register" />} />
+             <Route path="/register" element={<AuthPage initialView="register" />} />
             
             {/* Main Routes */}
             <Route path="/" element={<MainHome />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/book" element={<BookList />} />
             <Route path="/book/:id" element={<BookDetails />} />
@@ -62,8 +60,6 @@ root.render(
               </ProtectedRoute>
             } />
           </Routes>
-        </div>
-      </div>
     </BrowserRouter>
   </AppProvider>
 );
